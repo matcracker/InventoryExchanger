f484e707cef362833e752bc696de4401e90f8cee
matcracker <matcracker98@gmail.com>
2016-01-29 19:49:11 +0100

    Finished first rewrite, released beta 1

--- src/InventoryExchanger/matcracker/Main.php
+++ src/InventoryExchanger/matcracker/Main.php
@@ -1,7 +1,6 @@
 <?php
 namespace InventoryExchanger\matcracker;
 
-use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
@@ -10,7 +9,7 @@ use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 use pocketmine\item\Item;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase{
 
 	public function onEnable(){
 		$this->getLogger()->info(TextFormat::GREEN."InventoryExchanger is activated.");
@@ -22,44 +21,52 @@ class Main extends PluginBase implements Listener{
 	}
 	
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		$prefix = "[InventoryExchanger]";
+		$prefix = "§b[InventoryExchanger]";
 		$name = $sender->getName();
 
 		if(!$sender instanceof Player){
 			$sender->sendMessage($prefix . "Please run this command in-game");
 			return true;
+		}else{
+			if(strtolower($cmd->getName()) == "inventoryexchanger"){
+				if($sender->hasPermission("inventoryexchanger.command.inve")){
+					$inv = $sender->getInventory();
+					if (!isset($this->ie[$name])) $this->ie[$name] = [];
+					$getInv = [];
+
+					foreach ($inv->getContents() as $gI) {
+						if ($gI->getID() !== 0 and $gI->getCount() > 0) $getInv[] = [$gI->getID(), $gI->getDamage(), $gI->getCount()];
+					}
+
+					$setInv = [];
+					foreach ($this->ie[$name] as $iE)
+						$setInv[] = Item::get($iE[0], $iE[1], $iE[2]);
+
+					$this->ie[$name] = $getInv;
+					$inv->setContents($setInv);
+					$this->saveYml();
+
+					$sender->sendMessage($prefix . "§2You correctly change inventory!");
+
+				}else{
+					$sender->sendMessage("§4You don't have permission to use this command!");
+				}
+			}
 		}
-
-		$inv = $sender->getInventory();
-		if(!isset($ie[$name])) $ie[$name] = [];
-		$getInv = [];
-
-		foreach($inv->getContents() as $gI){
-			if($gI->getID() !== 0 and $gI->getCount() > 0) $getInv[] = [$gI->getID(),$gI->getDamage(),$gI->getCount() ];
-		}
-
-		$setInv = [];
-		foreach($ie[$name] as $iE)
-			$setInv[] = Item::get($iE[0], $iE[1], $iE[2]);
-
-		$ie[$name] = $getInv;
-		$inv->setContents($setInv);
-		$this->saveYml();
-
 		return true;
 	}
 
 	public function loadYml(){
 		@mkdir($this->getServer()->getDataPath() . "/plugins/InventoryExchanger/");
-		$config = new Config($this->getServer()->getDataPath() . "/plugins/InventoryExchanger/" . "config.yml", Config::YAML);
-		$ie = $config->getAll();
+		$inventories = new Config($this->getServer()->getDataPath() . "/plugins/InventoryExchanger" . "Inventories.yml", Config::YAML);
+		$this->ie = $inventories->getAll();
 	}
 
 	public function saveYml(){
-		$config = new Config($this->getServer()->getDataPath() . "/plugins/InventoryExchanger/" . "config.yml", Config::YAML);
-		asort($ie);
-		$config->setAll($ie);
-		$config->save();
+		$invetories = new Config($this->getServer()->getDataPath() . "/plugins/InventoryExchanger" . "Inventories.yml", Config::YAML);
+		asort($this->ie);
+		$invetories->setAll($this->ie);
+		$invetories->save();
 		$this->loadYml();
 	}
 
